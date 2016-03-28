04c61727ee397c768dec2a91630f0ca728963ba0
Benjamin Largent <benjamin.largent@gmail.com>
2016-03-28 14:00:13 -0700

    basic registering and login working

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,15 +17,17 @@ Route::group(['middleware' => ['web']], function () {
         return view('welcome');
     });
 
+    // Authentication routes...
+    Route::get('login', 'Auth\AuthController@getLogin');
+    Route::post('login', 'Auth\AuthController@postLogin');
+    Route::get('logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+    Route::get('register', 'Auth\AuthController@getRegister');
+    Route::post('register', 'Auth\AuthController@postRegister');
+
+
 });
 
 
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
